816381d1a9cf2222f8c11b452973813f545bbc21
dima <dima.squash123@gmail.com>
2018-10-10 10:17:44 +0300

    Initial Commit

---
+++ Theatre-project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Theatre-project
+//
+//  Created by dmitriy1 on 10.10.2018.
+//  Copyright © 2018 dmitriy1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
